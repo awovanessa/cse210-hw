9a24b53517e2b32f1216193a634fdeea24870d99
Vanessa <aviwebstudios@gmail.com>
2026-06-30 05:59:03 +0100

    completed excercise 1,2,3,4,, and 5

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,13 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        // ask the user for their name.
-        Console.Write("What is your first name? ");
-        string first = Console.ReadLine();
-
-        Console.Write("What is your last name? ");
-        string last = Console.ReadLine();
-
-        Console.WriteLine($"{last}, {first} {last}");
+        Console.WriteLine("Hello World! This is the Sandbox Project.");
     }
 }
